1ce3cffecc52e39746c1e0fc772ca9da75f166ad
Zahar <feelnoregret@gmail.com>
2026-02-09 15:51:00 +0300

    3d attempt to fix

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -20,7 +20,7 @@ export const UserList = (props: UserListPropsType) => {
     <div id={'hw01-users'}>
       <h2>User List:</h2>
 
-      <ul >
+      <ul>
         {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
           <li key={user.id} id={`hw01-user-${user.id}`}>
             <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
